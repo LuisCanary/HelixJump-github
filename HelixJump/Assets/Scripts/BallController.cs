2bc4fe9f1556d13cedad4ceb7861bbe63f98dc85
Luis <luisgomezruiz@gmail.com>
2019-08-17 14:58:20 +0200

    Added some prefabs, materials and ball script

---
+++ HelixJump/Assets/Scripts/BallController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallController : MonoBehaviour
+{
+
+    private bool ignoreNextCollision;
+
+    private Rigidbody rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+
+        rb.velocity = Vector3.zero;
+        rb.AddForce(Vector3.up,ForceMode.Impulse);
+    }
+
+
+}
